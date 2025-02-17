d86300abc14c998715555d4a3c4902d62a274ba4
Wenderson Pina <139186216+wederson77@users.noreply.github.com>
2025-02-17 19:56:52 -0300

    Update credentials.ts

--- credentials.ts
+++ credentials.ts
@@ -1,5 +1,5 @@
 export = {
-    sandbox: process.env.SANDBOX === "true",
+    sandbox: process.env.SANDBOX === "false",
     client_id: process.env.CLIENT_ID || "",
     client_secret: process.env.CLIENT_SECRET || "",
     pix_cert: "/etc/secrets/producao-680475-SDK-TS (1)",
