1efdeeafc7e935ae9f01b90151ef8ef13d991daf
Wenderson Pina <139186216+wederson77@users.noreply.github.com>
2025-02-17 18:43:34 -0300

    Update credentials.ts

--- credentials.ts
+++ credentials.ts
@@ -1,8 +1,6 @@
 export = {
-    // PRODUÇÃO = false
-    // HOMOLOGAÇÃO = true
-    sandbox: false,
-    client_id: 'Client_Id_dd53f452a72ef527cc615ed9ca72ac500d1d620b',
-    client_secret: 'Client_Secret_e7518ab2efb6d02df567e4e50d023f725d6f9041',
-    pix_cert: 'C:/Users/wende/OneDrive/Desktop/api-pix/src/producao-680475-SDK-TS (1).p12',
+    sandbox: process.env.SANDBOX === "true",
+    client_id: process.env.CLIENT_ID || "",
+    client_secret: process.env.CLIENT_SECRET || "",
+    pix_cert: "/etc/secrets/producao-680475-SDK-TS.p12",
 };
